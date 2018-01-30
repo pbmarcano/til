f53a9f7c24beaad0ca677bd6386ccd9bea9eb209
Peter Marcano <pbmarcano91@gmail.com>
2018-01-29 19:09:41 -0500

    Whoops! Fixed some comment styling

--- tailwind.js
+++ tailwind.js
@@ -270,7 +270,7 @@ module.exports = {
   | Here is where you define your font weights. We've provided a list of
   | common font weight names with their respective numeric scale values
   | to get you started. It's unlikely that your project will require
-   all of these, so we recommend removing those you don't need.
+  | all of these, so we recommend removing those you don't need.
   |
   | Class name: .font-{weight}
   |
